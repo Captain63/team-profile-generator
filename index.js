513ac2f81f472072ebc8b45054306974df3ee98c
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-04-21 20:43:45 -0400

    Add HTML formatting for writePage

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ const prompts = {
             {
                 type: "input",
                 message: "Enter manager's name:",
-                name: "managerID"
+                name: "managerName"
             },
             {
                 type: "number",
                 message: "Enter manager's ID:",
-                name: "managerNumber"
+                name: "managerID"
             },
             {
                 type: "email",
@@ -106,28 +106,91 @@ const writeToPage = () => {
     <html>
         <head>
             <title>Meet the Team</title>
+
+            <!-- Normalize CSS CDN link -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+
+            <!-- Bootstrap CDN link -->
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+            <!-- Font Awesome link -->
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
         </head>
-        <body>`;
+        <body>
+            <nav class="navbar p-3 bg-info">
+                <h1 class="text-center text-white w-100"><i class="fas fa-user-friends"></i> Meet the Team <i class="fas fa-user-friends"></i></h1>
+            </nav>
+
+            <div class="row px-3">`;
 
     const closingTags = `
+            </div>
         </body>
     </html>`;
 
     // Manager will always be required, so these tags are assumed for that class
-    let centerContent = "";
+    let centerContent = `
+    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+        <div class="my-3 card">
+            <div class="card-header bg-success text-white">
+                <h2 class="card-title">${managerResponses.getName()}</h2>
+                <h3 class="card-subtitle"><i class="fas fa-user-tie"></i> ${managerResponses.getRole()}</h3>  
+            </div>
+            <div class="card-body bg-light p-3">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${managerResponses.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${managerResponses.getEmail()}">${managerResponses.getEmail()}</a></li>
+                    <li class="list-group-item">Office Number: ${managerResponses.getOffice()}</li>
+                </ul>
+            </div>
+        </div>
+    </div>`;
 
     // Confirms there is more than 0 engineer objects -- 0 evaluates to false
     if (engineerResponses.length) {
         engineerResponses.forEach(engineer => {
             // Pulls values for engineer object into HTML tags
-            const engineerCard = "";
+            const engineerCard = `
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="my-3 card">
+                    <div class="card-header bg-success text-white">
+                        <h2 class="card-title">${engineer.getName()}</h2>
+                        <h3 class="card-subtitle"><i class="fas fa-user-cog"></i> ${engineer.getRole()}</h3>  
+                    </div>
+                    <div class="card-body bg-light p-3">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID: ${engineer.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGitHub()}" target="_blank">${engineer.getGitHub()}</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>`;
 
             // Concatenates engineerCard onto centerContent
             centerContent += engineerCard;
         })
-    } else if (internResponses.length) {
+    } 
+    
+    if (internResponses.length) {
         internResponses.forEach(intern => {
-            const internCard = "";
+            const internCard = `
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="my-3 card">
+                    <div class="card-header bg-success text-white">
+                        <h2 class="card-title">${intern.getName()}</h2>
+                        <h3 class="card-subtitle"><i class="fas fa-user-graduate"></i> ${intern.getRole()}</h3>  
+                    </div>
+                    <div class="card-body bg-light p-3">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID: ${intern.getId()}</li>
+                            <li class="list-group-item">Email: <a href="mailto: ${intern.getEmail()}">${intern.getEmail()}</a></li>
+                            <li class="list-group-item">School: ${intern.getSchool()}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>`;
 
             centerContent += internCard;
         })
