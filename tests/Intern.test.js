ea403662d698a8c004bd61ae5a50de096ba7ddb6
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-04-23 16:18:30 -0400

    Add constructor test case for Intern class

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,6 +1,17 @@
 const Intern = require("./../lib/Intern");
 
 describe("Intern class", () => {
+    describe("Constructor function", () => {
+        it("Sets object's name, id, email and school properties equal to provided arguments", () => {
+            const testIntern = new Intern("Stephen", 1, "test@test.com", "JMU");
+
+            expect(testIntern.name).toEqual("Stephen");
+            expect(testIntern.id).toEqual(1);
+            expect(testIntern.email).toEqual("test@test.com");
+            expect(testIntern.school).toEqual("JMU");
+        })
+    })
+
     describe("getSchool method", () => {
         it("Returns school property", () => {
             const testIntern = new Intern("Stephen", 1, "test@test.com", "JMU");
