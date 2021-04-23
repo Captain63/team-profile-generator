913fe1ea1b3d5d87590ec23dca92dc44e9ae1a47
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-04-23 16:16:22 -0400

    Add constructor test case for Engineer class

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,6 +1,17 @@
 const Engineer = require("./../lib/Engineer");
 
 describe("Engineer class", () => {
+    describe("Constructor function", () => {
+        it("Sets object's name, id, email and gitHub properties equal to provided arguments", () => {
+            const testEngineer = new Engineer("Stephen", 1, "test@test.com", "Captain63");
+
+            expect(testEngineer.name).toEqual("Stephen");
+            expect(testEngineer.id).toEqual(1);
+            expect(testEngineer.email).toEqual("test@test.com");
+            expect(testEngineer.gitHub).toEqual("Captain63");
+        })
+    })
+
     describe("getGitHub method", () => {
         it("Returns gitHub property", () => {
             const testEngineer = new Engineer("Stephen", 1, "test@test.com", "Captain63");
