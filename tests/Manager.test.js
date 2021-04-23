9d9bcca37d5c6e121ba06f7dafc7673206c72da1
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-04-23 16:20:48 -0400

    Add constructor test case for Manager class

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,6 +1,17 @@
 const Manager = require("./../lib/Manager");
 
 describe("Manager class", () => {
+    describe("Constructor function", () => {
+        it("Sets object's name, id, email and school properties equal to provided arguments", () => {
+            const testManager = new Manager("Stephen", 1, "test@test.com", 10);
+
+            expect(testManager.name).toEqual("Stephen");
+            expect(testManager.id).toEqual(1);
+            expect(testManager.email).toEqual("test@test.com");
+            expect(testManager.officeNumber).toEqual(10);
+        })
+    })
+
     describe("getOffice method", () => {
         it("Returns officeNumber property", () => {
             const testManager = new Manager("Stephen", 1, "test@test.com", 10);
